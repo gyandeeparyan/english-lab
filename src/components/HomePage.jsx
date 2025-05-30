7b61c69a67815b90f307a308a1cf81782447161f
Gyandeep Aryan <gyandeeparyan.dev@gmail.com>
2025-05-30 21:50:05 +0530

    Update HomePage grid layout for improved responsiveness

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -52,7 +52,7 @@ export const HomePage = () => {
       <h1 className='text-2xl font-bold mb-6 text-center'>
         English Conversation Lab 
       </h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+      <div className='grid grid-cols-1  md:grid-cols-4 lg:grid-cols-5 gap-4'>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((standard) => (
           <Link key={standard} to={`/class/${standard}`}>
             <Card
